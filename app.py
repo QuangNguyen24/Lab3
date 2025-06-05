e415164504fe42ce8fbb4014941f91b0c9aa6009
QuangNguyen24 <quang02102004@gmail.com>
2025-06-05 14:38:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from shop import app
+from flask import Flask, render_template
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
